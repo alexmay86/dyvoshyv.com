52a632dd520d8daef04b7c2d57b0367939904d81
eugeniaty <tytarenkoev@gmail.com>
2025-03-26 17:24:17 +0200

    Elementro Pro plugin

---
+++ wp-content/plugins/elementor-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/elementor-pro',
+        'pretty_version' => '3.28.x-dev',
+        'version' => '3.28.9999999.9999999-dev',
+        'reference' => '68f83af6e3fb4f482ad5bba0d470c6bef07549a4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/elementor-pro' => array(
+            'pretty_version' => '3.28.x-dev',
+            'version' => '3.28.9999999.9999999-dev',
+            'reference' => '68f83af6e3fb4f482ad5bba0d470c6bef07549a4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
